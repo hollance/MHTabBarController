ad0eaafe12a1af2725f6a9c6796f406d74a887a5
Angel Garcia <angelgarcia.mail@gmail.com>
2012-10-04 13:50:28 +0200

    Added ability to set views from Interface Builder with different layout

---
+++ Demo/MHTabBarController-Protected.h
@@ -0,0 +1,18 @@
+//
+//  MHTabBarController-Protected.h
+//  Pods
+//
+//  Created by Angel Garcia on 10/4/12.
+//
+//
+
+#import "MHTabBarController.h"
+
+@interface MHTabBarController ()
+
+@property(nonatomic, strong) IBOutlet UIView *tabButtonsContainerView;
+@property(nonatomic, strong) IBOutlet UIView *contentContainerView;
+@property(nonatomic, strong) IBOutlet UIImageView *indicatorImageView;
+
+
+@end
